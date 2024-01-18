195c86f327c51e05dfdc3be566540e58aeedc4d5
Md Riad <riad5089@gmail.com>
2024-01-18 14:47:07 +0600

    updated

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DimondPricePrediction',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='Riad',
+    author_email='riad5089@gmail.com',
     install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
